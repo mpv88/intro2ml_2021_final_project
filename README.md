65e9e630514d333ac95781f06b06aed7abb1f3b5
mattia <mattiapivi@gmail.com>
2022-01-19 02:32:34 +0100

    setup

---
+++ README.md
@@ -0,0 +1,28 @@
+# i2ml_2021_final_project
+nba-related prediction problems
+
+Deliver a single document (as a pdf file, written in English or Italian), within the exam date, by email, to the teacher. The document maximum length is fixed at 4 pages (excluding references), if the document is drafted according to this LaTex template, or 1200 words (including every word: title, authors, references, …), otherwise.
+https://www.overleaf.com/project/61df5902f8b32afb02235e6e
+
+The document will contain (not necessarily in the following order, not necessarily in a structure of sections corresponding to the following items):
+    0) the problem statement, that is a formal description of what is the information (input) upon which a decision/figure (output) has to be made/computed
+    1) a description of one or more performance indexes able to capture the degree to which a(ny) solution solves the problem, or some other evaluation criteria
+    2) a description of the proposed solution, from the algorithmic point of view, with zero or scarce focus on the tools used to implement it
+    3) a description of the experimental evaluation of the solution, including
+        -a description of the used data, if any
+        -a description of the experimental procedure and the comparison baseline, if any
+        -the presentation and the discussion of the results
+
+The students are allowed (and encouraged) to refer to existing (technical/scientific/research) literature for shrinking/omitting obvious parts of the description, if appropriate. The students are not required to deliver any source code or other material. However, if needed for gaining more insights on their work, the students will be asked to provide more material or to answer some questions. If the project has been done by a group of students, the document must show, for each student of the group, which (at least one) of the following activities the student took part in:
+   - problem statement
+   - solution design
+   - solution development
+   - data collection
+   - writing
+
+# Twitter food popularity
+The goal is to build a tool for predicting how popular will be a tweet about food. 
+There is no data available for this problem: the student is hence required to obtain it, if the proposed solution is based on data (likely it will be!).
+
+Sources:
+http://belindazeng.github.io/goingviral/
---
+++ main.py
@@ -0,0 +1,83 @@
+import tweepy as tw
+import pandas as pd
+import pickle
+
+def downloadTweets(api, users, file_name = 'all_tweets.pkl', min_number_of_followers=1e6):
+    ''' downloads number 'count' tweets from users listed in 'users' 
+
+        Parameters
+        ----------
+        api: twitter API object for download of tweets
+        users: list of target twitter accounts
+        file_name: name of file on which data are saved
+        min_number_of_followers: lower bound to the number of followers of the given users from which
+                                 one wants to download tweets
+
+        Returns
+        -------
+        
+    '''
+    
+    all_tweets = []
+    tot_tweets_dwlded = 0
+
+    # download tweets of given users
+    print("downloading...")
+    for user in users:
+
+        user_object = api.get_user(screen_name = user)
+
+        if user_object.followers_count >= min_number_of_followers:
+            tweets = tw.Cursor(api.user_timeline, screen_name = user, tweet_mode="extended").items(3200)
+            #tweets = api.user_timeline(screen_name = user, count=20)
+            new_tweets = []
+            for status in tweets:
+                new_tweets += [status]
+            
+            all_tweets.extend(new_tweets)
+            tweets_dwlded = len(new_tweets)
+            tot_tweets_dwlded += tweets_dwlded
+            print('\t user {} -- {} tweets downloaded'.format(user, tweets_dwlded))
+            print('\t\t we have {} tweets now...'.format(tot_tweets_dwlded))
+
+    # saving to JSON
+    print("saving tweets to file...")
+    with open(file_name,'wb') as file: 
+        pickle.dump(all_tweets, file)
+
+
+def readNames(file_txt):
+    ''' substitutes the date/time of creation of a tweet with a categorical variable indicate a time zone duringe the day
+        Parameters
+        ----------
+        file_txt: file containing names of interest
+
+        Returns
+        -------
+        names: list of the names contained in the given file
+    '''
+
+    # companies list
+    names = []
+    with open(file_txt) as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            names.append(line.strip())
+    
+    return names
+
+
+consumer_key = '3XLVvCdmXhpjZYi5pOverZgPL'
+consumer_secret = 'TPVT9NSnQbQbje0yUP1J0wpX0rZHbkBAmx1dhNnBMTSUk4rZZh'
+access_token = '1248331519204249600-ZE16yRQr6xjKt6JLXwgzr2YavcI0ZL'
+access_token_secret = 'JvMHU8aqokwJC0VUOegGTkIhUjY2YQywcsUJOLUOiTuiZ'
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+companies = readNames('intro2ml_2021_final_project\\food_brands.txt')
+downloadTweets(api, companies, file_name='intro2ml_2021_final_project\\all_tweets_moreUsers_1e5.pkl', min_number_of_followers=1e5)
+
+X = pd.read_pickle('intro2ml_2021_final_project\\all_tweets_moreUsers_1e5.pkl')

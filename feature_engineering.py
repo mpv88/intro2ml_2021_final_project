4e886851a40c244fadc8b2077982e0fa70b7f53a
mattia <mattiapivi@gmail.com>
2022-02-03 00:37:07 +0100

    fix list counter

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,18 +1,75 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 
-def to_binary(df, col, threshold=1000):
-    pass
+def y_to_binary(df, col, threshold):
+    df.loc[df[col] < threshold, col] = 0
+    df.loc[df[col] >= threshold, col] = 1
+    return df
+
+def X_to_drop(df, col):
+    return df.drop([col], axis = 1, inplace = True)
     
-def to_count(df, col):
-    pass
+def X_to_length(df, col):
+    df[col] = df[col].astype(str).str.len()
+    df.rename(columns={col: f'{col}_length'}, inplace = True)
 
+def X_to_weekday(df, col):
+    df[col] = pd.to_datetime(df[col]).dt.day_name().astype('category')
+    df.rename(columns={col: f'weekday'}, inplace = True)
+
+def X_to_hour(df, col):
+    df[col] = pd.to_datetime(df[col]).dt.hour.astype('category')
+    df.rename(columns={col: f'hour'}, inplace = True)
+    
+def X_to_count_list(df, col):
+    df[col] =  df[col].str.len()
+    df.rename(columns={col: f'{col}_count'}, inplace = True)
+#FIXME
 
 if __name__ == "__main__":
     
-    tweets_df = pd.read_csv('intro2ml_2021_final_project\\Data\\2k_sample.csv', encoding = 'utf-8')
+    tweet_df = pd.read_csv('intro2ml_2021_final_project\\Data\\dataset_2k.csv', encoding = 'utf-8')
+    sent_df = pd.read_csv('intro2ml_2021_final_project\\Data\\sentiment_by_sentence.csv', encoding = 'utf-8')
+    
+    # join 2 datasets
+    tweets_df = pd.concat([tweet_df, sent_df], axis=1)
     
     # transforms variable to binary
-    to_binary(tweets_df, variable, threshold=1000)
+    y_to_binary(tweets_df, 'retweets_count', threshold = 1000)
     
-    # transforms variable to count
-    to_count(tweets_df, variable)
+    # drop variables
+    X_to_drop(tweets_df, 'replies_count')
+    X_to_drop(tweets_df, 'likes_count')
+    X_to_drop(tweets_df, 'id')
+    X_to_drop(tweets_df, 'language')
+    X_to_drop(tweets_df, 'retweet')
+    
+    # transforms variable to length
+    X_to_length(tweets_df, 'tweet')
+    
+     # transforms date to weekday
+    X_to_weekday(tweets_df, 'date')
+
+    # transforms time to hour bins
+    X_to_hour(tweets_df, 'time')
+
+    # transforms list variables to count of their elements
+    X_to_count_list(tweets_df, 'mentions')
+    X_to_count_list(tweets_df, 'urls')
+    X_to_count_list(tweets_df, 'photos')
+    X_to_count_list(tweets_df, 'hashtags')
+    '''
+    # encode categorical/dummy variables
+    oht_enc = OneHotEncoder()
+    tweets_df['weekday'] = ord_enc.fit_transform(tweets_df['weekday'])
+    tweets_df['hour'] = ord_enc.fit_transform(tweets_df['hour'])
+    tweets_df['video'] = ord_enc.fit_transform(tweets_df['video'])
+    tweets_df['quote_url'] = ord_enc.fit_transform(tweets_df['quote_url'])
+    
+    # standardize numerical variables
+    sc = StandardScaler()
+    normed_train_data = pd.DataFrame(sc.fit_transform(training), columns = X.columns)
+    '''
+    # dump processed file 
+    tweets_df.to_csv('intro2ml_2021_final_project\\Data\\2k_sample_processed.csv', encoding = 'utf-8', index = False)
+    print(tweets_df.head(10))    
---
+++ models.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# a) load dataset
+tweets_df = pd.read_csv('intro2ml_2021_final_project\\Data\\2k_sample_processed.csv', encoding = 'utf-8')
+tweets_df.head()
+
+
+
+X=data[['sepal length', 'sepal width', 'petal length', 'petal width']]  # Features
+y=data['species']  # Labels
+
+
+# c) split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3) # 70% training and 30% test
+
+
+# d) train/predict Gaussian Classifier
+clf = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+                             max_depth=None, max_features='auto', max_leaf_nodes=None,
+                             min_impurity_decrease=0.0,
+                             min_samples_leaf=1, min_samples_split=2,
+                             min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=1,
+                             oob_score=False, random_state=None, verbose=0,
+                             warm_start=False)
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+# e) evaluate accuracy
+print("Confusion matrix", metrics.confusion_matrix(y_test, y_pred))
+print("Accuracy", metrics.accuracy_score(y_test, y_pred))
+
+# single prediction given inputs
+print(clf.predict([[3, 5, 4, 2]]))
+
+# f) evaluate variable importance & plot
+feature_imp = pd.Series(clf.feature_importances_, index = iris.feature_names).sort_values(ascending = False)
+print(feature_imp)
+
+# bar plot
+sns.barplot(x = feature_imp, y = feature_imp.index)
+plt.xlabel('Feature Importance Score')
+plt.ylabel('Features')
+plt.title('Visualizing Important Features')
+plt.legend()
+plt.show()
+
+# g) retrain the model just on 3 features
+X = data[['petal length', 'petal width','sepal length']]  # removed unuseful feature "sepal length"
+y = data['species']
+                                       
+# training/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.70, random_state = 5) # 70% train/30% test
+
+# fit/predict RF classifier
+clf=RandomForestClassifier(n_estimators = 100, oob_score = True)
+clf.fit(X_train , y_train)
+y_pred = clf.predict(X_test)
+
+# evaluate accuracy
+print("Confusion matrix 2", metrics.confusion_matrix(y_test, y_pred))
+print("Accuracy 2", metrics.accuracy_score(y_test, y_pred))
+#print (clf.score(X_train , y_train))
+#print(clf.score(y_test, y_pred))
+
+# OBB error
+oob_error = 1 - clf.oob_score_
+print(oob_error)
+
+# print ROC
+rfc_disp = metrics.RocCurveDisplay.from_estimator(clf, X_test, y_test, alpha=0.8)
+plt.show()
